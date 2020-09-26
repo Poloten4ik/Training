984cacda7e31f90c289ac384c1fdeca22b0010b6
Мурад <52107353+Poloten4ik@users.noreply.github.com>
2020-09-26 14:10:04 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    
+     
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sorting numSorting = new Sorting();
+
+            int[] x = new int[] { 5, 6, 4, 7, 2, 3, 1, 0, 10, 8, 9 };
+            int[] arrAsc = numSorting.OrderAsc(x);
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(arrAsc[i]);
+            }
+            int[] arrDesc = numSorting.OrderDesc(x);
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(arrDesc[i]);
+            }
+            Variant2 variant2 = new Variant2();
+            variant2.OrderedAsc(x);
+        }
+        
+    }
+
+   
+
+}
---
+++ Sorting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+public class Sorting
+{
+    public int[] OrderAsc(int[] x)
+    {
+        int result;
+        for (int i = 0; i < x.Length; i++)
+        {
+
+            for (int j = i + 1; j < x.Length; j++)
+            {
+
+                if (x[i] > x[j])
+                {
+                    result = x[i];
+                    x[i] = x[j];
+                    x[j] = result;
+                }
+            }
+        }
+        return x;
+
+    }
+    public int[] OrderDesc(int[] x)
+    {
+        int result;
+        for (int i = 0; i < x.Length; i++)
+        {
+
+            for (int j = i + 1; j < x.Length; j++)
+            {
+
+                if (x[i] < x[j])
+                {
+                    result = x[i];
+                    x[i] = x[j];
+                    x[j] = result;
+                }
+            }
+        }
+        return x;
+    }
+}
